0b136d6ab3d1579080ebcd0ec53b31a3333b546c
Marcel Widmer <marzelwidmer@gmail.com>
2019-02-25 21:54:17 +0100

    work on  puka puka

--- pukapuka-service/src/main/kotlin/ch/c3smonkey/pukapuka/PukaPukaServiceApplication.kt
+++ pukapuka-service/src/main/kotlin/ch/c3smonkey/pukapuka/PukaPukaServiceApplication.kt
@@ -1,8 +1,28 @@
 package ch.c3smonkey.pukapuka
 
+import brave.sampler.Sampler
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.server.HandlerFunction
+import org.springframework.web.reactive.function.server.RequestPredicate
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+import java.util.logging.Level
+import java.util.logging.Logger
+
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,3 +31,5 @@ class PukaPukaServiceApplication
 fun main(args: Array<String>) {
     runApplication<PukaPukaServiceApplication>(*args)
 }
+
+
